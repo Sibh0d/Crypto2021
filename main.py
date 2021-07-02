1f4c24890c99d19e3583f49b82fe424100f75670
Simcha Dov Berdy (He/Him) <86726582+Sibh0d@users.noreply.github.com>
2021-07-02 14:06:08 +0000

    Transferred functions to separate file.

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-def read_file(location):
-  file=open(location,"r")
-  words=[]
-  lines = file.readlines()
-  for l in lines:
-    words.extend(l.split())
-    #words[-1]+="\n"
-    words.append("\n")
-  return words
-def write_file(location,list_of_words):
-  file=open(location,"w")
-  for w in list_of_words:
-    #if w[-2:]!="\n":
-    if w !="\n":
-      file.write(w+" ")
-    else:
-      file.write(w)
-    #else:
-    #  file.write(w)
+from read_write import read_file,write_file
 print(read_file("Poem"))
 write_file("Poem2",read_file("Poem"))
