2e78ccf242c0e3bad6abd36df8e13599041a7ab5
Simcha Dov Berdy (He/Him) <86726582+Sibh0d@users.noreply.github.com>
2021-07-06 17:44:43 +0000

    Custom list of words

--- CrackingCaesar.py
+++ CrackingCaesar.py
@@ -1,20 +1,22 @@
-from nltk.corpus import brown
 from Caesar import decrypt_message
-from read_write import print_message
+from read_write import print_message,read_file
 def Print_All_Messages(T):
   for i in range(26):
     print("(Key: "+str(i)+") " + print_message(decrypt_message(T,i)))
 def Crack_Cipher(T):
   best=0
   holders=[]
-  words = brown.words()
+  words = read_file("Words") #brown.words()
   for i in range(26):
     print(i)
     current=0
-    for w in decrypt_message(T,i):
+    a=decrypt_message(T,i)
+    for w in a:
+      print(w)
       if w in words:
         current+=1
     current=current/len(T)
+    print(current)
     if current>best:
       best=current
       holders=[i]
@@ -22,3 +24,4 @@ def Crack_Cipher(T):
       holders.append(i)
   for h in holders:
     print(decrypt_message(T,h))
+    print(best)
--- main.py
+++ main.py
@@ -2,13 +2,4 @@ from read_write import read_file,write_file
 from Caesar import encrypt_letter, encrypt_word, encrypt_message,decrypt_letter,decrypt_word,decrypt_message
 from CrackingCaesar import Print_All_Messages,Crack_Cipher
 from CrackingRandom import Crack_Random
-#print(encrypt_message(read_file("Poem"),8))
-#print(decrypt_message(encrypt_message(read_file("Poem"),7),7))
-#print(read_file("Poem"))
-#write_file("Poem2",read_file("Poem"))
-#write_file("PT",decrypt_message(read_file("CT"),5))
-#write_file("CT",encrypt_message(read_file("PT"),19))
-#Print_All_Messages(["A","b","c"])
-Crack_Cipher(read_file("PT"))
-#Crack_Random(1)
-Print_All_Messages(read_file("PT"))
+Crack_Cipher(read_file("CT"))
